494b09e36f2d4acf5cd0f4a2a7dffebf3f784f36
shifat35 <23101142@uap-bd.edu>
2025-07-24 12:41:12 +0600

    Text file added by me

---
+++ P-1.py
@@ -0,0 +1,8 @@
+
+name = input("Welcome to the ATM! Please enter your name: ")
+
+
+amount = input("Hi " + name + "! How much money would you like to withdraw? $")
+
+
+print(f"Hello, {name}! Your ${amount} is ready for pickup. Stay safe!")
